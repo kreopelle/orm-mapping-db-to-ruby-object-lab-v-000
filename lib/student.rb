a9fc9e24fb78c29fcd795a72ebb0e6cc012fc95a
Kayla Reopelle <kreopelle@gmail.com>
2018-06-28 19:36:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
 
   def self.students_below_12th_grade
     sql = <<-SQL
-    SELECT * FROM students WHERE grade < 12
+    SELECT * FROM students WHERE grade NOT 12
     SQL
     DB[:conn].execute(sql)
   end
