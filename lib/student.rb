a06d12976934633f858023a5861ad9d79b2d2cf9
Kayla Reopelle <kreopelle@gmail.com>
2018-06-28 19:31:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,13 +16,12 @@ class Student
 
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * FROM students 
+      SELECT * FROM students
       WHERE name = ?
       LIMIT 1
     SQL
 
     DB[:conn].execute(sql, name)
-      
   end
 
   def save
