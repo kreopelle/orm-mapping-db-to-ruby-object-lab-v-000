6fd5d700951903fb1d9c5cc4009c3994ee110ce2
Kayla Reopelle <kreopelle@gmail.com>
2018-06-28 19:34:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,12 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students where grade = 9
+    SQL
+  end
+
   def self.new_from_db(row)
     student = self.new
     student.id = row[0]
@@ -16,7 +22,7 @@ class Student
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end 
+    end
   end
 
   def self.find_by_name(name)
